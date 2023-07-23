7c2bec6ccb852c330016181a74cd5d336a09230c
William Poon <willphk@outlook.com>
2023-07-22 20:09:33 -0400

    Added autocompletion

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,42 @@
+//Loading
 const recipeBtn = document.querySelector(".recipe-btn");
 const loading = document.querySelector(".loading");
 recipeBtn.addEventListener("click", () => {
   loading.style.display = "block";
   recipeBtn.style.display = "none";
 });
+
+//AutoComplete
+document
+  .getElementById("autoCompleteInput")
+  .addEventListener("input", function () {
+    let query = this.value;
+    if (query.length > 2) {
+      fetchSuggestions(query);
+    }
+  });
+
+function fetchSuggestions(query) {
+  fetch(`https://api.datamuse.com/sug?s=${query}&topics=food`)
+    .then((response) => response.json())
+    .then((data) => {
+      displaySuggestions(data);
+    });
+}
+
+function displaySuggestions(suggestions) {
+  let resultsDiv = document.getElementById("autoCompleteResults");
+  resultsDiv.innerHTML = "";
+
+  suggestions.forEach((suggestion) => {
+    let div = document.createElement("div");
+    div.innerHTML = suggestion.word;
+    div.addEventListener("click", function () {
+      document.getElementById("autoCompleteInput").value = suggestion.word;
+      resultsDiv.innerHTML = "";
+      resultsDiv.style.display = "none";
+    });
+    resultsDiv.appendChild(div);
+    resultsDiv.style.display = "block";
+  });
+}
